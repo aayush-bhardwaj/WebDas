6c81185e29cbd42baaa2b03154c20e8aad811e28
aayush-bhardwaj <aayush.bhj@gmail.com>
2017-01-29 11:47:18 +0530

    Adding Node.Js authentication passport module

---
+++ nodejsAuthentication/config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'facebookAuth' : {
+    'clientID' : '449942188672593',
+    'clientSecret' : 'f6a94918c853da329701e200dfed3b94',
+    'callbackURL':'http://localhost:8080/auth/facebook/callback'
+  }
+}
